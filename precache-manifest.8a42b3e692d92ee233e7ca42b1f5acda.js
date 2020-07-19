953971dec373a1e127353070dde4127637f84ec9
aman dubey <dubey.amansus@gmail.com>
2020-07-19 03:48:47 -0700

    Updates

---
+++ precache-manifest.8a42b3e692d92ee233e7ca42b1f5acda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48a02a45eaf71a18669a",
+    "url": "/adesh/static/css/main.f1bba5ee.chunk.css"
+  },
+  {
+    "revision": "48a02a45eaf71a18669a",
+    "url": "/adesh/static/js/main.48a02a45.chunk.js"
+  },
+  {
+    "revision": "f84815d47b2a187dde32",
+    "url": "/adesh/static/js/1.f84815d4.chunk.js"
+  },
+  {
+    "revision": "a4a5759b3872552c4df7",
+    "url": "/adesh/static/js/runtime~main.a4a5759b.js"
+  },
+  {
+    "revision": "53cfcf57b6ab25fc8d051a5a629a21d5",
+    "url": "/adesh/index.html"
+  }
+];
